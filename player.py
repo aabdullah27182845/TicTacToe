52d421c0e37c71689b26b47eccef6a6a006e8362
aabdullah27182845 <102864939+aabdullah27182845@users.noreply.github.com>
2022-07-16 14:39:16 +0200

    Add files via upload
    added a decision making computer class for the tic tac toe player. (Not yet perfect)

--- player.py
+++ player.py
@@ -40,3 +40,55 @@ class HumanPlayer(Player):
                 print("Invalid square")
 
         return val
+
+
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.availableMoves()) == 9:
+            square = random.choice(game.availableMoves())
+        else:
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = '0' if player == 'X' else 'X'
+
+        if state.currentWinner == other_player:
+            return {'position': None,
+                    'score': ((state.num_empty_squares() + 1) if other_player == max_player else  \
+                    -1 * (state.num_empty_squares() + 1))}
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+
+        for possible_move in state.availableMoves():
+
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)
+
+            state.board[possible_move] = ' '
+            state.currentWinner = None
+            sim_score['position'] = possible_move
+
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+
+        return best 
+        
+
+
+
+
